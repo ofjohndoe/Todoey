225831d9573a2ec5f586b7f20b99fbdd178d66f0
Sevban Kocabaş <sevbankocabas@icloud.com>
2018-07-03 16:09:40 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Sevban Kocabaş on 3.07.2018.
+//  Copyright © 2018 Sevban Kocabaş. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
